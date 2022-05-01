9ce97600e2a27d88704c4e1ff458aa7d587e38c5
littleshepherd <beelthor@gmail.com>
2022-05-01 13:31:31 -0500

    agregamos la parte de contacto

--- js/login.js
+++ js/login.js
@@ -1,57 +1,91 @@
 const form = document.getElementById('form-login');
-window.onload = ()=>{
-// login
-const login = async (jsonSend) => {
-    const response = await fetch('https://projectherramientas.herokuapp.com/project_herramientas/login', {
-        method: 'POST',
-        headers: { 'content-type': 'application/json'},
-        body: JSON.stringify(jsonSend)
-    });
-    console.log(response.status)
-    if (response.status== 201){
-        window.location.href ='panel.html';
+window.onload = () => {
+    // login
+    const login = async (jsonSend) => {
+        const response = await fetch('https://projectherramientas.herokuapp.com/project_herramientas/login', {
+            method: 'POST',
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify(jsonSend)
+        });
+        console.log(response.status)
+        if (response.status == 201) {
+            window.location.href = 'panel.html';
+        }
     }
+
+    form.onsubmit = (e) => {
+        e.preventDefault();
+        let email = document.getElementById('email')
+        let password = document.getElementById('password')
+        email = email.value
+        password = password.value
+        let jsonSend = {
+            user: email,
+            password: password
+        }
+        login(jsonSend);
+
+    }
+
+    // register
+    const register = async (jsonSend) => {
+        const response = await fetch('https://projectherramientas.herokuapp.com/project_herramientas/register', {
+            method: 'POST',
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify(jsonSend)
+        });
+        console.log(response.status)
+        if (response.status == 201) {
+            window.location.href = 'panel.html';
+        }
+    }
+    const formRegister = document.getElementById('form-register');
+    formRegister.onsubmit = (e) => {
+        e.preventDefault();
+        let name = document.getElementById('name-register');
+        let email = document.getElementById('email-register');
+        let password = document.getElementById('password-register');
+        name = name.value
+        email = email.value
+        password = password.value
+        let jsonSend = {
+            name: name,
+            email: email,
+            password: password
+        }
+    }
+
+    // contact
+    const contact = async (jsonSend) => {
+        const response = await fetch('https://projectherramientas.herokuapp.com/project_herramientas/contact', {
+            method: 'POST',
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify(jsonSend)
+        });
+        console.log(response.status)
+        if (response.status == 201) {
+            window.location.href = 'panel.html';
+        }
+    }
+    const formContact = document.getElementById('form-contact');
+    formContact.onsubmit = (e) => {
+        e.preventDefault();
+        let name = document.getElementById('contact-name');
+        let email = document.getElementById('contact-email'); 
+        let phone = document.getElementById('contact-phone');
+        let message = document.getElementById('contact-message');
+        name = name.value
+        email = email.value
+        phone = phone.value
+        message = message.value
+        let jsonSend = {
+            name: name,
+            email: email,
+            phone: phone,
+            message: message
+        }
+    }
+
 }
 
-form.onsubmit= (e)=>{
-    e.preventDefault();
-    let email = document.getElementById('email')
-    let password = document.getElementById('password')
-    email = email.value
-    password = password.value
-    let jsonSend = {
-        user: email,
-        password: password
-    }
-    login(jsonSend);
 
-}
-
-// register
-const register = async (jsonSend) => {
-    const response = await fetch('https://projectherramientas.herokuapp.com/project_herramientas/register', {
-        method: 'POST',
-        headers: { 'content-type': 'application/json'},
-        body: JSON.stringify(jsonSend)
-    });
-    console.log(response.status)
-    if (response.status== 201){
-        window.location.href ='panel.html';
-    }
-}
-const formRegister = document.getElementById('form-register');
-formRegister.onsubmit= (e)=>{
-    e.preventDefault();
-    let name = document.getElementById('name-register');
-    let email = document.getElementById('email-register');
-    let password = document.getElementById('password-register');
-    name = name.value
-    email = email.value
-    password = password.value
-    let jsonSend = {
-        name: name,
-        email: email,
-        password: password
-    }
-              }
-}
